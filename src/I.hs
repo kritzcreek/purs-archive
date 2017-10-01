46d4710e5f4b76c7bfdf51f0348b29847443fde5
kritzcreek <christoph.hegemann1337@gmail.com>
2017-10-01 18:28:35 +0200

    adds helper functions to batch download a bunch of bower files for experiments

--- src/I.hs
+++ src/I.hs
@@ -2,22 +2,29 @@ module I where
 
 -- GHCI helpers
 
-import Protolude
+import           Protolude
 
-import Control.Lens ((^.), (?~))
-import Data.Aeson as A
-import qualified Data.Aeson.Types as A
 import qualified Codec.Archive.Tar as Tar
+import           Control.Concurrent.Async
+import           Control.Lens ((^.), (?~))
+import           Data.Aeson as A
+import qualified Data.Aeson.Types as A
+import qualified Data.ByteString.Base64 as Base64
+import qualified Data.HashMap.Strict as HM
+import           Data.Pool (Pool, destroyAllResources)
+import qualified Data.SemVer as SemVer
+import qualified Data.Text.Encoding as TE
+import qualified Data.Vector as V
+import           Database.Persist.Sqlite (SqlBackend)
+import qualified GitHub as GH
+import qualified GitHub.Endpoints.Repos as GH
+import qualified GitHub.Endpoints.Repos.Contents as GH
+import           Manifest
 import qualified Network.Wreq as W
 import qualified Persist.Sqlite as Lite
-import Data.Pool (Pool, destroyAllResources)
-import Database.Persist.Sqlite (SqlBackend)
+import           System.Directory
+import           System.FilePath
 import qualified User as User
-import Manifest
-import qualified Data.SemVer as SemVer
-import qualified Data.HashMap.Strict as HM
-import System.Directory
-import System.FilePath
 
 registerDummy :: Pool SqlBackend -> IO Bool
 registerDummy pool = registerUser pool "creek@gmail.com" "hunter2"
@@ -81,3 +88,74 @@ convertBowerToToml fp version' = do
         version <- either (const mzero) pure . SemVer.fromText =<< pure version'
         deps <- package .:? "dependencies" .!= HM.empty
         pure (Manifest name version ">= 0.11.5" ["author"] (HM.toList deps))
+
+-- | Downloads all bower files for the repositories specified in `repos`. You
+-- | have to provide a valid GitHub access token for `oauth`, and add the
+-- | additional repos you want to download to the `repos` value.
+downloadZeBowerFiles :: IO ()
+downloadZeBowerFiles =
+  for_ repos $ \repo -> do
+    putText ("Downloading tags for: " <> show repo)
+    tags <- fetchTags repo
+    forConcurrently_ tags $ \tag ->
+      unlessM (doesDirectoryExist (mkBowerPath (snd repo) tag)) $ do
+        putText ("Downloading bower file for: " <> show repo <> show (GH.tagName tag))
+        writeBowerJson repo tag =<< decodeContent <$> downloadBowerJson repo tag
+
+---------------
+-- | Customize these:
+---------------
+oauth :: GH.Auth
+oauth = GH.OAuth ""
+
+repos :: [(GH.Name GH.Owner, GH.Name GH.Repo)]
+repos = map (bimap mkOwner (mkRepo . (<>) "purescript-"))
+  [ ("purescript", "prelude")
+  , ("purescript", "either")
+  , ("purescript", "maybe")
+  , ("purescript", "arrays")
+  , ("purescript", "strings")
+  , ("purescript", "lists")
+  , ("purescript", "control")
+  ]
+
+bowerFileDirectory :: FilePath
+bowerFileDirectory = "bowerfiles"
+---------------
+-- | End Customize
+---------------
+
+mkOwner :: Text -> GH.Name GH.Owner
+mkOwner = GH.mkName (Proxy :: Proxy GH.Owner)
+
+mkRepo :: Text -> GH.Name GH.Repo
+mkRepo = GH.mkName (Proxy :: Proxy GH.Repo)
+
+fetchTags :: (GH.Name GH.Owner, GH.Name GH.Repo) -> IO (V.Vector GH.Tag)
+fetchTags (owner, repo) = do
+  tags <- GH.tagsFor' (Just oauth) owner repo
+  case tags of
+    Left err -> panic ("Failed to fetch tags for: " <> GH.untagName owner <> "/" <> GH.untagName repo <> " with: " <> show err)
+    Right tags' -> pure tags'
+
+downloadBowerJson :: (GH.Name GH.Owner, GH.Name GH.Repo) -> GH.Tag -> IO GH.ContentFileData
+downloadBowerJson (owner, repo) tag = do
+  content <- GH.contentsFor' (Just oauth) owner repo "bower.json" (Just (GH.tagName tag))
+  case content of
+    Left err -> panic ("Failed to bower.json for: " <> GH.untagName owner <> "/" <> GH.untagName repo <> ":" <> GH.tagName tag <> " with: " <> show err)
+    Right (GH.ContentFile content') -> pure content'
+    _ -> panic "wot? bower.json was a directory"
+
+decodeContent :: GH.ContentFileData -> Text
+decodeContent fileContent =
+  let decodeBase64 = TE.decodeUtf8 . Base64.decodeLenient . TE.encodeUtf8
+  in decodeBase64 (GH.contentFileContent fileContent)
+
+writeBowerJson :: (GH.Name GH.Owner, GH.Name GH.Repo) -> GH.Tag -> Text -> IO ()
+writeBowerJson (_, repo) tag content = do
+  let dirName = mkBowerPath repo tag
+  createDirectoryIfMissing True dirName
+  writeFile (dirName </> "bower.json") content
+
+mkBowerPath :: GH.Name GH.Repo -> GH.Tag -> FilePath
+mkBowerPath repo tag = bowerFileDirectory  </> toS (GH.untagName repo) </> toS (GH.tagName tag)
